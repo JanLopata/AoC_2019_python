7d6db7042129da0a270fddd8108f4545dda31d42
Jan Princ <jan.lopata@rohlik.cz>
2022-07-17 19:18:57 +0200

    AoC 2019 Day06 part II

--- day06.py
+++ day06.py
@@ -2,17 +2,23 @@ import os
 
 
 def part1(data: str):
+    direct_orbit_map = parse_direct_orbit_map(data)
+
+    counter = 0
+    for start in direct_orbit_map.keys():
+        counter += count_indirect(start, direct_orbit_map)
+    return counter
+
+
+def parse_direct_orbit_map(data):
     direct_orbit_map = {}
     for row in data.split("\n"):
         if len(row) == 0:
             continue
         orbit_info = row.split(")")
         direct_orbit_map[orbit_info[1]] = orbit_info[0]
+    return direct_orbit_map
 
-    counter = 0
-    for start in direct_orbit_map.keys():
-        counter += count_indirect(start, direct_orbit_map)
-    return counter
 
 def count_indirect(start, direct_orbit_map):
     pointer = start
@@ -24,8 +30,28 @@ def count_indirect(start, direct_orbit_map):
     return counter
 
 
+def get_full_path(start, direct_orbit_map):
+    pointer = start
+    path = []
+    while pointer in direct_orbit_map:
+        path.append(pointer)
+        pointer = direct_orbit_map[pointer]
+
+    path.append(pointer)
+    return path
+
+
 def part2(data: str):
-    return -1
+    direct_orbit_map = parse_direct_orbit_map(data)
+
+    you_path = get_full_path("YOU", direct_orbit_map)
+    san_path = get_full_path("SAN", direct_orbit_map)
+
+    while you_path[-1] == san_path[-1]:
+        you_path.pop()
+        san_path.pop()
+
+    return len(you_path) + len(san_path) - 2
 
 
 def read_data():
@@ -37,7 +63,6 @@ if __name__ == "__main__":
     this_filename = os.path.basename(__file__)
     input_filename = os.path.join("input", this_filename.replace("day", "").replace(".py", ".txt"))
     data = read_data()
-    # print(input_filename)
 
     print(part1(data))
     print(part2(data))
--- test06.py
+++ test06.py
@@ -18,10 +18,20 @@ J)K
 K)L
 """
 
+part2_test_data_addon = """
+K)YOU
+I)SAN
+"""
+
 class AoCTest(unittest.TestCase):
     def test_part1(self):
         result = day.part1(small_test_data)
         self.assertEqual(42, result)
+
+    def test_part2(self):
+        result = day.part2(small_test_data + part2_test_data_addon)
+        self.assertEqual(4, result)
+
 
 if __name__ == '__main__':
     unittest.main()
