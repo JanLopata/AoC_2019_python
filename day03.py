ff1366b9736bbf201c765ddef07aebac8b978b1d
Jan Princ <jan.lopata@rohlik.cz>
2022-12-02 22:33:46 +0100

    AoC 2022 - Day 3 prep

--- day03.py
+++ day03.py
@@ -1,65 +1,18 @@
 import os
-from functools import reduce
+
+from aoc_tools import get_data
 
 
 def part1(data: str):
-    lines = data.split("\n")
-    first_wire_dict = get_wire_path_dict(lines[0])
-    second_wire_dict = get_wire_path_dict(lines[1])
-    return reduce(lambda x, y: min(x, y),
-                  map(lambda pos: abs(pos[0]) + abs(pos[1]),
-                      set(first_wire_dict.keys()).intersection(set(second_wire_dict.keys()))
-                      ))
+    pass
 
 
 def part2(data: str):
-    lines = data.split("\n")
-    first_wire_dict = get_wire_path_dict(lines[0])
-    second_wire_dict = get_wire_path_dict(lines[1])
-    return reduce(lambda x, y: min(x, y),
-                  map(lambda pos: first_wire_dict[pos] + second_wire_dict[pos],
-                      set(first_wire_dict.keys()).intersection(set(second_wire_dict.keys()))
-                      ))
-
-
-def get_wire_path_dict(csv):
-    x = 0
-    y = 0
-    res = {}
-    step = 0
-
-    for instruction in csv.split(","):
-        direction = instruction[0]
-        value = int(instruction[1:])
-        xdiff = 0
-        ydiff = 0
-        if direction == 'R':
-            xdiff = 1
-        if direction == 'L':
-            xdiff = -1
-        if direction == 'D':
-            ydiff = 1
-        if direction == 'U':
-            ydiff = -1
-
-        for i in range(value):
-            x += xdiff
-            y += ydiff
-            step += 1
-            res[(x, y)] = step
-
-    return res
-
-
-def read_data():
-    with open(input_filename) as input_file:
-        return input_file.read()
+    pass
 
 
 if __name__ == "__main__":
-    this_filename = os.path.basename(__file__)
-    input_filename = os.path.join("input", this_filename.replace("day", "").replace(".py", ".txt"))
-    # print(input_filename)
+    data = get_data(os.path.basename(__file__))
 
-    print(part1(read_data()))
-    print(part2(read_data()))
+    print(part1(data))
+    print(part2(data))
--- test03.py
+++ test03.py
@@ -1,25 +1,18 @@
 import unittest
 
-from parameterized import parameterized
-
 import day03 as day
+
+test_data = """
+"""
 
 
 class AoCTest(unittest.TestCase):
 
-    @parameterized.expand([
-        ["R8,U5,L5,D3\nU7,R6,D4,L4", 6],
-        ["R75,D30,R83,U83,L12,D49,R71,U7,L72\nU62,R66,U55,R34,D71,R55,D58,R83", 159],
-        ["R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51\nU98,R91,D20,R16,D67,R40,U7,R15,U6,R7", 135]])
-    def test_part1(self, wires, expected):
-        self.assertEqual(expected, day.part1(wires))
+    def test_part1(self):
+        self.assertEqual(1, day.part1(test_data))
 
-    @parameterized.expand([
-        ["R8,U5,L5,D3\nU7,R6,D4,L4", 30],
-        ["R75,D30,R83,U83,L12,D49,R71,U7,L72\nU62,R66,U55,R34,D71,R55,D58,R83", 610],
-        ["R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51\nU98,R91,D20,R16,D67,R40,U7,R15,U6,R7", 410]])
-    def test_part2(self, wires, expected):
-        self.assertEqual(expected, day.part2(wires))
+    def test_part2(self):
+        self.assertEqual(2, day.part2(test_data))
 
 
 if __name__ == '__main__':
