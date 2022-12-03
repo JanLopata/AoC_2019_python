4c9e4db2ddf8b2bda0d29f937810eba4e8a7e5af
Jan Princ <jan.lopata@rohlik.cz>
2022-12-03 06:26:27 +0100

    AoC 2022 - Day 3

--- day03.py
+++ day03.py
@@ -4,11 +4,63 @@ from aoc_tools import get_data
 
 
 def part1(data: str):
-    pass
+    lines = data.split("\n")
+
+    value_sum = 0
+
+    for line in lines:
+        if line == "":
+            continue
+
+        # half of the line
+        half = len(line) // 2
+        first_half = line[:half]
+        second_half = line[half:]
+        first_set = set([x for x in first_half])
+        second_set = set([x for x in second_half])
+
+        intsec = first_set.intersection(second_set)
+        ch = intsec.pop()
+        value_sum += get_character_value(ch)
+
+    return value_sum
 
 
 def part2(data: str):
-    pass
+    lines = data.split("\n")
+    found = set()
+
+    value_sum = 0
+    counter = 0
+
+    for line in lines:
+
+        if line == "":
+            continue
+
+        counter += 1
+
+        if counter == 1:
+            found = set([x for x in line])
+        else:
+            found = found.intersection(set([x for x in line]))
+
+        if counter == 3:
+            counter = 0
+            ch = found.pop()
+            value = get_character_value(ch)
+
+            value_sum += value
+
+    return value_sum
+
+
+def get_character_value(ch):
+    if ch.isupper():
+        value = ord(ch) - ord('A') + 1 + 26
+    else:
+        value = ord(ch) - ord('a') + 1
+    return value
 
 
 if __name__ == "__main__":
--- test03.py
+++ test03.py
@@ -3,16 +3,22 @@ import unittest
 import day03 as day
 
 test_data = """
+vJrwpWtwJgWrhcsFMMfFFhFp
+jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL
+PmmdzqPrVvPwwTWBwg
+wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn
+ttgJtRGJQctTZtZT
+CrZsJsPPZsGzwwsLwLmpwMDw
 """
 
 
 class AoCTest(unittest.TestCase):
 
     def test_part1(self):
-        self.assertEqual(1, day.part1(test_data))
+        self.assertEqual(157, day.part1(test_data))
 
     def test_part2(self):
-        self.assertEqual(2, day.part2(test_data))
+        self.assertEqual(70, day.part2(test_data))
 
 
 if __name__ == '__main__':
