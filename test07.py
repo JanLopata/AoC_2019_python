69af32e79fc37fcbfa87ec25aebaaa21ee0abdbe
Jan Princ <jan.lopata@rohlik.cz>
2022-12-07 08:35:30 +0100

    AoC 2022 - Day 7

--- test07.py
+++ test07.py
@@ -1,33 +1,40 @@
 import unittest
 
 import day07 as day
-
-test_data_part1 = """
-3,15,3,16,1002,16,10,16,1,16,15,15,4,15,99,0,0;43210
-3,23,3,24,1002,24,10,24,1002,23,-1,23,101,5,23,23,1,24,23,23,4,23,99,0,0;54321
-3,31,3,32,1002,32,10,32,1001,31,-2,31,1007,31,0,33,1002,33,7,33,1,33,31,31,1,32,31,31,4,31,99,0,0,0;65210
-"""
-
-test_data_part2 = """
-3,26,1001,26,-4,26,3,27,1002,27,2,27,1,27,26,27,4,27,1001,28,-1,28,1005,28,6,99,0,0,5;139629729
-3,52,1001,52,-5,52,3,53,1,52,56,54,1007,54,5,55,1005,55,26,1001,54,-5,54,1105,1,12,1,53,54,53,1008,54,0,55,1001,55,1,55,2,53,55,53,4,53,1001,56,-1,56,1005,56,6,99,0,0,0,0,10;18216
+test_data = """
+$ cd /
+$ ls
+dir a
+14848514 b.txt
+8504156 c.dat
+dir d
+$ cd a
+$ ls
+dir e
+29116 f
+2557 g
+62596 h.lst
+$ cd e
+$ ls
+584 i
+$ cd ..
+$ cd ..
+$ cd d
+$ ls
+4060174 j
+8033020 d.log
+5626152 d.ext
+7214296 k
 """
 
 
 class AoCTest(unittest.TestCase):
+
     def test_part1(self):
-        for line in test_data_part1.splitlines():
-            if line == "":
-                continue
-            result = day.part1(line.split(";")[0])
-            self.assertEqual(int(line.split(";")[1]), result)
+        self.assertEqual(95437, day.part1(test_data))
 
     def test_part2(self):
-        for line in test_data_part2.splitlines():
-            if line == "":
-                continue
-            result = day.part2(line.split(";")[0])
-            self.assertEqual(int(line.split(";")[1]), result)
+        self.assertEqual(24933642, day.part2(test_data))
 
 
 if __name__ == '__main__':
