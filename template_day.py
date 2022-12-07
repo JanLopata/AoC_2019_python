55233d1bbdfd6e1f486cff3ad3f65ad9daf7af0d
Jan Princ <jan.lopata@rohlik.cz>
2022-12-07 08:44:04 +0100

    AoC 2022 - Add templates

---
+++ template_day.py
@@ -0,0 +1,18 @@
+import os
+
+from aoc_tools import get_data
+
+
+def part1(data: str):
+    pass
+
+
+def part2(data: str):
+    pass
+
+
+if __name__ == "__main__":
+    data = get_data(os.path.basename(__file__))
+
+    print(part1(data))
+    print(part2(data))
---
+++ template_test.py
@@ -0,0 +1,28 @@
+import unittest
+
+from parameterized import parameterized
+
+import dayXX as day
+
+test_data = """
+"""
+
+
+class AoCTest(unittest.TestCase):
+
+    def test_part1(self):
+        self.assertEqual(-1, day.part1(test_data))
+
+    @parameterized.expand([
+        ["1234", 1],
+    ])
+    def test_parametrized_part1(self, data, expected):
+        result = day.part1(data)
+        self.assertEqual(expected, result)
+
+    def test_part2(self):
+        self.assertEqual(-1, day.part2(test_data))
+
+
+if __name__ == '__main__':
+    unittest.main()
