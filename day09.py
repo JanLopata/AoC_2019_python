b34c273d9497925f2c4a51783ba20f2c8684e96a
Jan Princ <jan.lopata@rohlik.cz>
2022-12-09 07:37:28 +0100

    AoC 2022 - Day 9

--- day09.py
+++ day09.py
@@ -1,37 +1,129 @@
+import math
 import os
 
-from intcode_computer import IntcodeComputer
+from aoc_tools import get_data
+
+directions = {
+    'U': (0, -1),
+    'D': (0, 1),
+    'L': (-1, 0),
+    'R': (1, 0),
+}
+
+PRINT_SIZE = 14
+debug_print = False
+
+
+def simulate(head, tail, delta_x, delta_y):
+    head = (head[0] + int(delta_x), head[1] + int(delta_y))
+
+    # distance between head and tail
+    dist_x = head[0] - tail[0]
+    dist_y = head[1] - tail[1]
+    dist2 = dist_x ** 2 + dist_y ** 2
+    if dist2 >= 4:
+        # move tail
+        if dist_x * dist_y == 0:
+            # head is on the same line as tail
+            if dist_x == 0:
+                # head is on the same vertical line as tail
+                tail = (tail[0], tail[1] + int(math.copysign(1, dist_y)))
+            else:
+                # head is on the same horizontal line as tail
+                tail = (tail[0] + int(math.copysign(1, dist_x)), tail[1])
+
+        else:
+            # head is on the diagonal line
+            tail = (tail[0] + math.copysign(1, dist_x), tail[1] + math.copysign(1, dist_y))
+
+    return head, tail
+
+
+def compute_delta(point1, point2):
+    return int(point1[0] - point2[0]), int(point1[1] - point2[1])
 
 
 def part1(data: str):
-    program = [int(x) for x in data.split(",")]
-    computer = IntcodeComputer()
-    computer.import_program(program)
-    computer.accept_input([1])
-    print(computer.compute_while_possible())
-    print(computer.program, computer.position)
-    return computer.output
+    lines = data.split("\n")
+
+    head = (0, 0)
+    tail = (0, 0)
+    visits = set()
+    visits.add(tail)
+
+    for line in lines:
+        if line == "":
+            continue
+        sp = line.split(" ")
+        direction = sp[0]
+        count = int(sp[1])
+
+        for _ in range(count):
+            delta = directions[direction]
+            head, tail = simulate(head, tail, delta[0], delta[1])
+            visits.add(tail)
+
+    return len(visits)
+
+
+def print_situation(knots, size):
+    grid = [["."] * (2 * size + 1) for _ in range(-size, size + 1)]
+    grid[size][size] = "s"
+    for i in range(len(knots) - 1, -1, -1):
+        x = knots[i][0] + size
+        y = knots[i][1] + size
+        char = str(i)
+        if i == 0:
+            char = "H"
+        grid[y][x] = char
+
+    # print grid
+    for row in grid:
+        print("".join(row))
+
+    print()
 
 
 def part2(data: str):
-    program = [int(x) for x in data.split(",")]
-    computer = IntcodeComputer()
-    computer.import_program(program)
-    computer.accept_input([2])
-    print(computer.compute_while_possible())
-    print(computer.program, computer.position)
-    return computer.output
+    lines = data.split("\n")
 
+    knots = [(0, 0)] * 10
+    visits = set()
+    if debug_print:
+        print_situation(knots, PRINT_SIZE)
 
-def read_data():
-    with open(input_filename) as input_file:
-        return input_file.read()
+    for line in lines:
+        if line == "":
+            continue
+        sp = line.split(" ")
+        direction = sp[0]
+        count = int(sp[1])
+
+        for _ in range(count):
+
+            delta = directions[direction]
+            for i in range(len(knots) - 1):
+                head = knots[i]
+                tail = knots[i + 1]
+                head, tail = simulate(head, tail, delta[0], delta[1])
+                delta = compute_delta(tail, knots[i + 1])
+                knots[i] = head
+                # do not move the tail, it will be moved by the next call of simulate
+                if delta == (0, 0):
+                    break
+
+            # move the tail last time
+            knots[-1] = (knots[-1][0] + delta[0], knots[-1][1] + delta[1])
+            visits.add(knots[-1])
+
+        if debug_print:
+            print_situation(knots, PRINT_SIZE)
+
+    return len(visits)
 
 
 if __name__ == "__main__":
-    this_filename = os.path.basename(__file__)
-    input_filename = os.path.join("input", this_filename.replace("day", "").replace(".py", ".txt"))
-    data = read_data()
+    data = get_data(os.path.basename(__file__))
 
     print(part1(data))
     print(part2(data))
--- test09.py
+++ test09.py
@@ -2,22 +2,39 @@ import unittest
 
 import day09 as day
 
-test_data_part1 = """
-109,2000,109,19,1102,1,102030,1985,204,-34,99;102030
-109,1,204,-1,1001,100,1,100,1008,100,16,101,1006,101,0,99;109,1,204,-1,1001,100,1,100,1008,100,16,101,1006,101,0,99
-1102,34915192,34915192,7,4,7,99,0;1219070632396864
-104,1125899906842624,99;1125899906842624
+test_data = """
+R 4
+U 4
+L 3
+D 1
+R 4
+D 1
+L 5
+R 2
+"""
+
+larger_data = """
+R 5
+U 8
+L 8
+D 3
+R 17
+D 10
+L 25
+U 20
 """
 
 
 class AoCTest(unittest.TestCase):
+
     def test_part1(self):
-        for line in test_data_part1.splitlines():
-            if line == "":
-                continue
-            result = day.part1(line.split(";")[0])
-            interpreted_result = ",".join(str(it) for it in result)
-            self.assertEqual(line.split(";")[1], interpreted_result)
+        self.assertEqual(13, day.part1(test_data))
+
+    def test_part2_1(self):
+        self.assertEqual(1, day.part2(test_data))
+
+    def test_part2_2(self):
+        self.assertEqual(36, day.part2(larger_data))
 
 
 if __name__ == '__main__':
