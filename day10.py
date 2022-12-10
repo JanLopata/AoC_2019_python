2fe9a678ef34b2fe1eb4fab9c824483c7aa11229
Jan Princ <jan.lopata@rohlik.cz>
2022-12-10 08:21:23 +0100

    AoC 2022 - Day 10

--- day10.py
+++ day10.py
@@ -3,12 +3,75 @@ import os
 from aoc_tools import get_data
 
 
+class ClockCpu:
+    def __init__(self):
+        self.cycle = 1
+        self.cycles_memory = {}
+        self.value_x = 1
+        self.log_frequency = 1
+        self.log_start = 1
+
+    def run(self, instruction):
+        self.log_cycle()
+
+        if instruction == "noop":
+            self.cycle += 1
+            self.log_cycle()
+            return
+
+        sp = instruction.split(" ")
+
+        self.cycle += 1
+        self.log_cycle()
+        self.cycle += 1
+        self.value_x += int(sp[1])
+        self.log_cycle()
+
+    def log_cycle(self):
+
+        if (self.cycle - self.log_start) % self.log_frequency == 0:
+            self.cycles_memory[self.cycle] = self.value_x
+
+
+def draw_image(cycles_memory):
+    image = [["." for _ in range(42)] for _ in range(6)]
+
+    for cycle in range(1, 6 * 40 + 1):
+        if cycle not in cycles_memory:
+            continue
+        sprite_main_x = cycles_memory[cycle]
+        sprite_pixels = [sprite_main_x + i for i in range(-1, 2)]
+        drawn_x = (cycle - 1) % 40
+        y = (cycle - 1) // 40
+        if drawn_x in sprite_pixels:
+            image[y][drawn_x + 1] = "#"
+
+    image_string = "\n"
+    for row in image:
+        image_string += "".join(row[1:-1]) + "\n"
+    return image_string
+
+
 def part1(data: str):
-    pass
+    cpu = ClockCpu()
+    cpu.log_start = 20
+    cpu.log_frequency = 40
+    for instruction in data.splitlines():
+        if instruction == "":
+            continue
+        cpu.run(instruction)
+
+    return sum([key * value for key, value in cpu.cycles_memory.items()])
 
 
 def part2(data: str):
-    pass
+    cpu = ClockCpu()
+    for instruction in data.splitlines():
+        if instruction == "":
+            continue
+        cpu.run(instruction)
+
+    return draw_image(cpu.cycles_memory)
 
 
 if __name__ == "__main__":
--- test10.py
+++ test10.py
@@ -5,24 +5,180 @@ from parameterized import parameterized
 import day10 as day
 
 test_data = """
+noop
+addx 3
+addx -5
+"""
 
+larger_data = """
+addx 15
+addx -11
+addx 6
+addx -3
+addx 5
+addx -1
+addx -8
+addx 13
+addx 4
+noop
+addx -1
+addx 5
+addx -1
+addx 5
+addx -1
+addx 5
+addx -1
+addx 5
+addx -1
+addx -35
+addx 1
+addx 24
+addx -19
+addx 1
+addx 16
+addx -11
+noop
+noop
+addx 21
+addx -15
+noop
+noop
+addx -3
+addx 9
+addx 1
+addx -3
+addx 8
+addx 1
+addx 5
+noop
+noop
+noop
+noop
+noop
+addx -36
+noop
+addx 1
+addx 7
+noop
+noop
+noop
+addx 2
+addx 6
+noop
+noop
+noop
+noop
+noop
+addx 1
+noop
+noop
+addx 7
+addx 1
+noop
+addx -13
+addx 13
+addx 7
+noop
+addx 1
+addx -33
+noop
+noop
+noop
+addx 2
+noop
+noop
+noop
+addx 8
+noop
+addx -1
+addx 2
+addx 1
+noop
+addx 17
+addx -9
+addx 1
+addx 1
+addx -3
+addx 11
+noop
+noop
+addx 1
+noop
+addx 1
+noop
+noop
+addx -13
+addx -19
+addx 1
+addx 3
+addx 26
+addx -30
+addx 12
+addx -1
+addx 3
+addx 1
+noop
+noop
+noop
+addx -9
+addx 18
+addx 1
+addx 2
+noop
+noop
+addx 9
+noop
+noop
+noop
+addx -1
+addx 2
+addx -37
+addx 1
+addx 3
+noop
+addx 15
+addx -21
+addx 22
+addx -6
+addx 1
+noop
+addx 2
+addx 1
+noop
+addx -10
+noop
+noop
+addx 20
+addx 1
+addx 2
+addx 2
+addx -6
+addx -11
+noop
+noop
+noop
+"""
+
+expected_image = """
+##..##..##..##..##..##..##..##..##..##..
+###...###...###...###...###...###...###.
+####....####....####....####....####....
+#####.....#####.....#####.....#####.....
+######......######......######......####
+#######.......#######.......#######.....
 """
 
 
 class AoCTest(unittest.TestCase):
 
     def test_part1(self):
-        self.assertEqual(-1, day.part1(test_data))
+        self.assertEqual(0, day.part1(test_data))
 
-    @parameterized.expand([
-        ["1234", 1],
-    ])
-    def test_parametrized_part1(self, data, expected):
-        result = day.part1(data)
-        self.assertEqual(expected, result)
+    def test_part1_larger(self):
+        self.assertEqual(13140, day.part1(larger_data))
 
     def test_part2(self):
-        self.assertEqual(-1, day.part2(test_data))
+        self.assertEqual(expected_image, day.part2(larger_data))
 
 
 if __name__ == '__main__':
