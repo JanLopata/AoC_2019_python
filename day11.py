94f0cb97659b301596ab7357adb29df34473f1bf
Jan Princ <jan.lopata@rohlik.cz>
2022-12-11 09:51:53 +0100

    AoC 2022 - Day 11

--- day11.py
+++ day11.py
@@ -1,125 +1,134 @@
+import math
 import os
 
-from intcode_computer import IntcodeComputer, StoppingCondition
+from aoc_tools import get_data
 
 
-class PaintingRobot:
+def parse_monkey(block):
+    monkey = Monkey()
+    for line in block.split("\n"):
+        if line.startswith("Monkey"):
+            monkey.id = int(line.split(" ")[1][:-1])
 
+        if line.startswith("  Starting"):
+            sp = line.split(":")
+            monkey.items = [int(x) for x in sp[1].strip().split(", ")]
+
+        if line.startswith("  Operation"):
+            sp = line.split(":")
+            monkey.operation_parameters = sp[1].strip().split(" ")
+
+        if line.startswith("  Test"):
+            sp = line.split(":")
+            monkey.divisor = int(sp[1].strip().split(" ")[2])
+
+        if line.startswith("    If true"):
+            sp = line.split(":")
+            monkey.targets[1] = int(sp[1].strip().split(" ")[3])
+
+        if line.startswith("    If false"):
+            sp = line.split(":")
+            monkey.targets[0] = int(sp[1].strip().split(" ")[3])
+
+    monkey.build_operation()
+    return monkey
+
+
+def part1(data: str):
+    blocks = data.split("\n\n")
+    monkeys = []
+    for block in blocks:
+        monkey = parse_monkey(block)
+        monkeys.append(monkey)
+
+    lcm = compute_monkeys_lcm(monkeys)
+
+    for i in range(20):
+        do_round(monkeys, lcm)
+
+    inspections = [monkey.inspect_counter for monkey in monkeys]
+    inspections.sort()
+    return inspections[-1] * inspections[-2]
+
+
+def do_round(monkeys, lcm):
+    for monkey in monkeys:
+
+        shuffles = []
+        while len(monkey.items) > 0:
+            shuffles.append(monkey.process_one_item())
+
+        for move in shuffles:
+            target = monkeys[move[0]]
+            received_item = move[1] % lcm
+            target.items.append(received_item)
+
+
+def compute_monkeys_lcm(monkeys):
+    # compute least common multiple of all divisors
+    divisors = [monkey.divisor for monkey in monkeys]
+    lcm = divisors[0]
+    for i in range(1, len(divisors)):
+        lcm = lcm * divisors[i] // math.gcd(lcm, divisors[i])
+    return lcm
+
+
+def part2(data: str):
+    blocks = data.split("\n\n")
+    monkeys = []
+    for block in blocks:
+        monkey = parse_monkey(block)
+        monkeys.append(monkey)
+
+    for monkey in monkeys:
+        monkey.bored_divisor = 1
+
+    lcm = compute_monkeys_lcm(monkeys)
+
+    for i in range(10000):
+
+        if i % 100 == 0:
+            print("Progress: {}%".format(i / 100))
+        do_round(monkeys, lcm)
+
+    inspections = [monkey.inspect_counter for monkey in monkeys]
+    inspections.sort()
+    return inspections[-1] * inspections[-2]
+
+
+class Monkey:
     def __init__(self):
-        self.debug_mode = False
-        self.direction_idx = 0
-        self.directions = [(0, -1), (-1, 0), (0, 1), (1, 0)]
-        self.position = (0, 0)
-        self.environment = set()
-        self.visited = set()
+        self.id = None
+        self.items = []
+        self.operation_parameters = []
+        self.operation_lambda = None
+        self.divisor = None
+        self.targets = [-1, -1]
+        self.bored_divisor = 3
+        self.inspect_counter = 0
 
-    def move(self):
-        direction = self.get_direction()
-        new_position = (self.position[0] + direction[0], self.position[1] + direction[1])
-        if self.debug_mode:
-            print("Moving {} -> {} by {}, dir. {}".format(self.position, new_position, direction, self.direction_idx))
-        self.position = new_position
+    def __str__(self):
+        return f"Monkey {self.id} with items {self.items}, OpPars: {self.operation_parameters}, OpLambda: {self.operation_lambda}, Divisor: {self.divisor}, Targets: {self.targets}"
 
-    def get_direction(self):
-        return self.directions[self.direction_idx]
+    def build_operation(self):
+        right_side = " ".join(self.operation_parameters[2:])
+        self.operation_lambda = (lambda old: eval(right_side))
 
-    def turn(self, turn_number):
-        increment = -1 if turn_number == 1 else 1
-        new_direction = (self.direction_idx + increment) % len(self.directions)
-        if self.debug_mode:
-            print("Turning to {} by {}".format(new_direction, turn_number))
-        self.direction_idx = new_direction
+    def process_one_item(self):
+        self.inspect_counter += 1
+        worry = self.items.pop(0)
 
-    def paint(self, color_number):
-        if self.debug_mode:
-            print("Painting on {} to {}".format(self.position, color_number))
-        if color_number == 1:
-            self.environment.add(self.position)
+        worry = self.operation_lambda(worry)
+        # worry = hardcoded_ops_test(worry, self.id)
+        if self.bored_divisor != 1:
+            worry = worry // self.bored_divisor
 
-        if color_number == 0 and self.position in self.environment:
-            self.environment.remove(self.position)
-
-        self.visited.add(self.position)
-
-    def get_color_number(self):
-        return 1 if self.position in self.environment else 0
-
-
-def run_robot(computer, partial_output, robot):
-    stopping_condition = None
-    while stopping_condition != StoppingCondition.END_OF_PROGRAM:
-
-        stopping_condition = computer.compute_while_possible()
-        if stopping_condition == StoppingCondition.EXPECTING_INPUT:
-            # read color
-            computer.accept_input([robot.get_color_number()])
-
-        if stopping_condition == StoppingCondition.STOP_ON_OUTPUT:
-            partial_output.append(computer.output[-1])
-            if len(partial_output) == 1:
-                robot.paint(partial_output[0])
-            if len(partial_output) >= 2:
-                robot.turn(partial_output[1])
-                robot.move()
-                partial_output.clear()
-
-
-def init_robot(input_program):
-    program = [int(x) for x in input_program.split(",")]
-    computer = IntcodeComputer()
-    computer.import_program(program)
-    computer.set_stop_on_output(True)
-    # this encapsulation is bad
-    # - the intcode computer should be inside of robot
-    # - the environment should be outside of robot
-    robot = PaintingRobot()
-    return computer, robot
-
-
-def max_size(tuples_list: list):
-    return max(abs(x) for sub_tuple in tuples_list for x in sub_tuple)
-
-
-def print_environment(environment):
-    max_range = max_size(environment)
-    result = ""
-    for i in range(-max_range, max_range):
-        for j in range(-max_range, max_range):
-            c = 'X' if (j, i) in environment else ' '
-            result += c
-        result += "\n"
-
-    print(result)
-
-
-def part1(input_program: str):
-    computer, robot = init_robot(input_program)
-    partial_output = []
-
-    run_robot(computer, partial_output, robot)
-
-    return len(robot.visited)
-
-
-def part2(input_program: str):
-    computer, robot = init_robot(input_program)
-    robot.environment.add((0, 0))
-    partial_output = []
-
-    run_robot(computer, partial_output, robot)
-
-    print_environment(robot.environment)
-
-
-def read_data():
-    with open(input_filename) as input_file:
-        return input_file.read()
+        target_monkey = self.targets[worry % self.divisor == 0]
+        return target_monkey, worry
 
 
 if __name__ == "__main__":
-    this_filename = os.path.basename(__file__)
-    input_filename = os.path.join("input", this_filename.replace("day", "").replace(".py", ".txt"))
-    data = read_data()
+    data = get_data(os.path.basename(__file__))
 
     print(part1(data))
     print(part2(data))
--- test11.py
+++ test11.py
@@ -2,31 +2,44 @@ import unittest
 
 import day11 as day
 
-test_data_part1 = """
-109,2000,109,19,1102,1,102030,1985,204,-34,99;102030
-109,1,204,-1,1001,100,1,100,1008,100,16,101,1006,101,0,99;109,1,204,-1,1001,100,1,100,1008,100,16,101,1006,101,0,99
-1102,34915192,34915192,7,4,7,99,0;1219070632396864
-104,1125899906842624,99;1125899906842624
+test_data = """
+Monkey 0:
+  Starting items: 79, 98
+  Operation: new = old * 19
+  Test: divisible by 23
+    If true: throw to monkey 2
+    If false: throw to monkey 3
+
+Monkey 1:
+  Starting items: 54, 65, 75, 74
+  Operation: new = old + 6
+  Test: divisible by 19
+    If true: throw to monkey 2
+    If false: throw to monkey 0
+
+Monkey 2:
+  Starting items: 79, 60, 97
+  Operation: new = old * old
+  Test: divisible by 13
+    If true: throw to monkey 1
+    If false: throw to monkey 3
+
+Monkey 3:
+  Starting items: 74
+  Operation: new = old + 3
+  Test: divisible by 17
+    If true: throw to monkey 0
+    If false: throw to monkey 1
 """
-
-
-def max_size(data: list):
-    return max(abs(x) for sublist in data for x in sublist)
 
 
 class AoCTest(unittest.TestCase):
 
-    def test_max_size(self):
-        data = [(-20, 4), (-24, 12), (21, 6), (0, 7), (-3, -4)]
-        self.assertEqual(24, max_size(data))
-
     def test_part1(self):
-        for line in test_data_part1.splitlines():
-            if line == "":
-                continue
-            result = day.part1(line.split(";")[0])
-            interpreted_result = ",".join(str(it) for it in result)
-            self.assertEqual(line.split(";")[1], interpreted_result)
+        self.assertEqual(10605, day.part1(test_data))
+
+    def test_part2(self):
+        self.assertEqual(2713310158, day.part2(test_data))
 
 
 if __name__ == '__main__':
