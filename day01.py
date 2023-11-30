bb0a208f1c35f75b9f02b3146ac8eb94c4368130
Jan Princ <jan.lopata@rohlik.cz>
2023-11-30 19:03:56 +0100

    AoC 2019 - Day 1 - use aoc_tools

--- day01.py
+++ day01.py
@@ -1,5 +1,7 @@
 import os
 
+from aoc_tools import get_data
+
 
 def compute_fuel_req(mass: int):
     return int(mass / 3) - 2
@@ -14,28 +16,24 @@ def compute_combined_fuel_req(mass):
     return result
 
 
-def part1():
-    with open(input_filename) as input_file:
-        result = 0
-        for line in input_file:
-            result += compute_fuel_req(int(line))
+def part1(data: str):
+    result = 0
+    for line in data.splitlines():
+        result += compute_fuel_req(int(line))
 
-        print(result)
+    return result
 
 
-def part2():
-    with open(input_filename) as input_file:
-        result = 0
-        for line in input_file:
-            result += compute_combined_fuel_req(int(line))
+def part2(data: str):
+    result = 0
+    for line in data.splitlines():
+        result += compute_combined_fuel_req(int(line))
 
-        print(result)
+    return result
 
 
 if __name__ == "__main__":
-    this_filename = os.path.basename(__file__)
-    input_filename = os.path.join("input", this_filename.replace("day", "").replace(".py", ".txt"))
-    # print(input_filename)
+    input_data = get_data(os.path.basename(__file__))
 
-    part1()
-    part2()
+    print(part1(input_data))
+    print(part2(input_data))
