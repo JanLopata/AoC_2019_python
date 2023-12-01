38731b861abdc30f1345d01e245939ece6c7236d
Jan Princ <jan.lopata@rohlik.cz>
2023-12-01 20:35:39 +0100

    AoC 2023 - day02 prep

--- day02.py
+++ day02.py
@@ -1,56 +1,31 @@
 import os
 
+from aoc_tools import get_data
 
-def run_program(program: list):
-    program = [x for x in program]
-    pos = 0
-    while program[pos] != 99:
-
-        if program[pos] == 1:
-            program[program[pos + 3]] = program[program[pos + 1]] + program[program[pos + 2]]
-            pos += 4
-
-        if program[pos] == 2:
-            program[program[pos + 3]] = program[program[pos + 1]] * program[program[pos + 2]]
-            pos += 4
-
-    return program
+debug_part1 = False
+debug_part2 = False
 
 
-def part1():
-    program_str = read_file_lines(input_filename)[0]
-    program_input = [int(x) for x in program_str.split(",")]
-    program_input[1] = 12
-    program_input[2] = 2
-    print(run_program(program_input)[0])
 
+def part1(data):
+    pass
 
-def part2():
-    program_str = read_file_lines(input_filename)[0]
-    base_program_input = [int(x) for x in program_str.split(",")]
+def part2(data):
+    pass
 
-    for i in range(2000):
-        for j in range(2000):
-            program = [x for x in base_program_input]
-            program[1] = i
-            program[2] = j
-            try:
-                if run_program(program)[0] == 19690720:
-                    print(100 * i + j)
-                    return
-            except IndexError:
-                pass
-
-
-def read_file_lines(filename):
-    with open(input_filename) as input_file:
-        return [x for x in input_file]
+def do_tests():
+    testdata1 = """
+    """
+    testdata2 = """
+    """
+    print(part1(testdata1))
+    print(part2(testdata2))
 
 
 if __name__ == "__main__":
-    this_filename = os.path.basename(__file__)
-    input_filename = os.path.join("input", this_filename.replace("day", "").replace(".py", ".txt"))
-    # print(input_filename)
+    input_data = get_data(os.path.basename(__file__))
 
-    part1()
-    part2()
+    do_tests()
+
+    print(part1(input_data))
+    print(part2(input_data))
