4b705df1a1f34d007ea066284baa7af906c7bed8
Jan Princ <jan.lopata@rohlik.cz>
2023-12-03 20:08:02 +0100

    AoC 2023 - day04 prep

--- day04.py
+++ day04.py
@@ -1,70 +1,31 @@
-def part1(data: str):
-    pair = data.split("-")
-    low = int(pair[0])
-    high = int(pair[1])
+import os
 
-    counter = 0
-    for n in range(low, high + 1):
-        if check_criterias(n):
-            counter += 1
+from aoc_tools import get_data
 
-    return counter
+debug_part1 = False
+debug_part2 = False
 
 
-def part2(data: str):
-    pair = data.split("-")
-    low = int(pair[0])
-    high = int(pair[1])
+def part1(data):
+    result = 0
 
-    counter = 0
-    for n in range(low, high + 1):
-        if additional_crit(n):
-            counter += 1
-
-    return counter
+    return result
 
 
-def is_double(tup):
-    return tup[1] - tup[0] == 2
+def part2(data):
+    pass
 
 
-def check_criterias(n):
-    digits = [int(n / 10 ** (5 - x)) % 10 for x in range(6)]
-    has_double = False
-    for i in range(len(digits) - 1):
-        if digits[i + 1] < digits[i]:
-            return False
-
-        if not has_double:
-            has_double = digits[i + 1] == digits[i]
-
-    return has_double
-
-
-def additional_crit(n):
-    digits = [int(n / 10 ** (5 - x)) % 10 for x in range(6)]
-
-    repeats = []
-    active = 0
-
-    for i in range(len(digits) - 1):
-        if digits[i + 1] < digits[i]:
-            return False
-
-        if digits[i + 1] != digits[active]:
-            repeats.append((active, i + 1))
-            active = i + 1
-
-    repeats.append((active, len(digits)))
-    doubles = [x for x in filter(is_double, repeats)]
-    return len(doubles) > 0
+def do_tests():
+    testdata1 = """"""
+    print(part1(testdata1))
+    print(part2(testdata1))
 
 
 if __name__ == "__main__":
-    # this_filename = os.path.basename(__file__)
-    # input_filename = os.path.join("input", this_filename.replace("day", "").replace(".py", ".txt"))
-    data = "156218-652527"
-    # print(input_filename)
+    input_data = get_data(os.path.basename(__file__))
 
-    print(part1(data))
-    print(part2(data))
+    do_tests()
+
+    print(part1(input_data))
+    print(part2(input_data))
