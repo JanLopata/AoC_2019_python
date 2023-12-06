e32d31365c0bfdae61a70c15e117fe76228cb51b
Jan Princ <jan.lopata@rohlik.cz>
2023-12-06 09:08:10 +0100

    AoC 2023 - day06

--- day06.py
+++ day06.py
@@ -1,58 +1,71 @@
+import math
 import os
 
+from aoc_tools import get_data
 
-def part1(data: str):
-    direct_orbit_map = parse_direct_orbit_map(data)
-
-    counter = 0
-    for start in direct_orbit_map.keys():
-        counter += len(get_full_path(start, direct_orbit_map)) - 1
-    return counter
+debug_part1 = False
+debug_part2 = False
 
 
-def parse_direct_orbit_map(data):
-    direct_orbit_map = {}
-    for row in data.split("\n"):
-        if len(row) == 0:
-            continue
-        orbit_info = row.split(")")
-        direct_orbit_map[orbit_info[1]] = orbit_info[0]
-    return direct_orbit_map
+def make_figures(r):
+    print("Max duration:", r)
+    for x in range(r + 1):
+        print("{}\t{}".format(x, x * r - x * x))
+    print("============")
 
 
-def get_full_path(start, direct_orbit_map):
-    pointer = start
-    path = []
-    while pointer in direct_orbit_map:
-        path.append(pointer)
-        pointer = direct_orbit_map[pointer]
-
-    path.append(pointer)
-    return path
+EPSILON = 1e-7
 
 
-def part2(data: str):
-    direct_orbit_map = parse_direct_orbit_map(data)
+def part1(data):
+    result = 1
 
-    you_path = get_full_path("YOU", direct_orbit_map)
-    san_path = get_full_path("SAN", direct_orbit_map)
+    input_rows = data.splitlines()
+    durations = [int(x) for x in input_rows[0].split(": ")[1].split()]
+    distances = [int(x) for x in input_rows[1].split(": ")[1].split()]
 
-    while you_path[-1] == san_path[-1]:
-        you_path.pop()
-        san_path.pop()
+    for i in range(len(durations)):
+        distance = distances[i]
+        duration = durations[i]
 
-    return len(you_path) + len(san_path) - 2
+        if debug_part1:
+            make_figures(duration)
+
+        ways_to_win = compute_ways_to_win(distance, duration)
+
+        if ways_to_win > 0:
+            result *= ways_to_win
+
+    return result
 
 
-def read_data():
-    with open(input_filename) as input_file:
-        return input_file.read()
+def compute_ways_to_win(distance, duration):
+    min_win = (duration - math.sqrt(duration * duration - 4 * distance)) / 2 + EPSILON
+    max_win = (duration + math.sqrt(duration * duration - 4 * distance)) / 2 - EPSILON
+    ways_to_win = math.floor(max_win) - math.ceil(min_win) + 1
+    return ways_to_win
+
+
+def part2(data):
+    input_rows = data.splitlines()
+    duration = int(input_rows[0].replace(" ", "").split(":")[1])
+    distance = int(input_rows[1].replace(" ", "").split(":")[1])
+
+    return compute_ways_to_win(distance, duration)
+
+
+def do_tests():
+    testdata1 = """Time:      7  15   30
+Distance:  9  40  200
+"""
+    print(part1(testdata1))
+    print(part2(testdata1))
 
 
 if __name__ == "__main__":
-    this_filename = os.path.basename(__file__)
-    input_filename = os.path.join("input", this_filename.replace("day", "").replace(".py", ".txt"))
-    data = read_data()
+    input_data = get_data(os.path.basename(__file__))
 
-    print(part1(data))
-    print(part2(data))
+    do_tests()
+
+    print(part1(input_data))
+    print(part2(input_data))
