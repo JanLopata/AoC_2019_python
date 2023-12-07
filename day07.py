866203b3c758b2b944bd70bb82c8415e941c171d
Jan Princ <jan.lopata@rohlik.cz>
2023-12-07 09:02:19 +0100

    AoC 2023 - day07 part I

--- day07.py
+++ day07.py
@@ -6,11 +6,94 @@ debug_part1 = False
 debug_part2 = False
 
 
+def map_card_to_number(card_char):
+    if card_char.isnumeric():
+        return int(card_char)
+
+    if card_char == "T":
+        return 10
+    if card_char == "J":
+        return 11
+    if card_char == "Q":
+        return 12
+    if card_char == "K":
+        return 13
+    if card_char == "A":
+        return 14
+
+
+def compute_strength(card_numbers: list):
+    d = {}
+    for c in card_numbers:
+        if c in d:
+            d[c] += 1
+        else:
+            d[c] = 1
+
+    v = d.values()
+    if max(v) == 5:
+        return 7
+
+    if max(v) == 4:
+        return 6
+
+    if max(v) == 3:
+        if len(v) == 2:
+            return 5  # full-house
+        else:
+            return 4
+
+    if max(v) == 2:
+        if len(v) == 3:
+            return 3  # two-pair
+        else:
+            return 2
+
+    return 1
+
+
+class HandOfCards:
+
+    def __init__(self, line: str):
+        line_split = line.split()
+        self.cards_str = line_split[0]
+        self.cards = [map_card_to_number(x) for x in line_split[0]]
+        self.bet = int(line_split[1])
+        self.strength = compute_strength(self.cards)
+        self.comparison_key = self.compute_comparison_key()
+        self.rank = None
+
+    def __str__(self):
+        return "{} bet {} strength {} cards_n {} compkey {}".format(self.cards_str, self.bet, self.strength, self.cards,
+                                                                    self.comparison_key)
+
+    def compute_comparison_key(self):
+        k = self.strength
+        for c in self.cards:
+            k *= 100
+            k += c
+        return k
+
+    def set_rank(self, val):
+        self.rank = val
+
+
 def part1(data):
     result = 0
 
+    hands = []
     for line in data.splitlines():
-        pass
+        hand = HandOfCards(line)
+        hands.append(hand)
+        # print(hand)
+
+    hands.sort(key=lambda h: h.comparison_key)
+
+    for i in range(len(hands)):
+        hand = hands[i]
+        hand.set_rank(i + 1)
+        # print(i, hand)
+        result += hand.rank * hand.bet
 
     return result
 
@@ -20,7 +103,11 @@ def part2(data):
 
 
 def do_tests():
-    testdata1 = """"""
+    testdata1 = """32T3K 765
+T55J5 684
+KK677 28
+KTJJT 220
+QQQJA 483"""
     print(part1(testdata1))
     print(part2(testdata1))
 
--- test07.py
+++ test07.py
@@ -1,33 +1,21 @@
 import unittest
 
+from parameterized import parameterized
+
 import day07 as day
-
-test_data_part1 = """
-3,15,3,16,1002,16,10,16,1,16,15,15,4,15,99,0,0;43210
-3,23,3,24,1002,24,10,24,1002,23,-1,23,101,5,23,23,1,24,23,23,4,23,99,0,0;54321
-3,31,3,32,1002,32,10,32,1001,31,-2,31,1007,31,0,33,1002,33,7,33,1,33,31,31,1,32,31,31,4,31,99,0,0,0;65210
-"""
-
-test_data_part2 = """
-3,26,1001,26,-4,26,3,27,1002,27,2,27,1,27,26,27,4,27,1001,28,-1,28,1005,28,6,99,0,0,5;139629729
-3,52,1001,52,-5,52,3,53,1,52,56,54,1007,54,5,55,1005,55,26,1001,54,-5,54,1105,1,12,1,53,54,53,1008,54,0,55,1001,55,1,55,2,53,55,53,4,53,1001,56,-1,56,1005,56,6,99,0,0,0,0,10;18216
-"""
 
 
 class AoCTest(unittest.TestCase):
-    def test_part1(self):
-        for line in test_data_part1.splitlines():
-            if line == "":
-                continue
-            result = day.part1(line.split(";")[0])
-            self.assertEqual(int(line.split(";")[1]), result)
-
-    def test_part2(self):
-        for line in test_data_part2.splitlines():
-            if line == "":
-                continue
-            result = day.part2(line.split(";")[0])
-            self.assertEqual(int(line.split(";")[1]), result)
+    @parameterized.expand([
+        ["32T3K 0", 2],
+        ["T55J5 0", 4],
+        ["KK677 0", 3],
+        ["KTJJT 0", 3],
+        ["QQQJA 0", 4],
+    ])
+    def test_hand_strength(self, inp, expected):
+        hand = day.HandOfCards(inp)
+        self.assertEqual(expected, hand.strength)
 
 
 if __name__ == '__main__':
