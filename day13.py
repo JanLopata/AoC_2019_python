d7bc0d5c9ab273e973f1ba8653a4f0a1cb1ee305
Jan Princ <jan.lopata@rohlik.cz>
2023-12-13 13:28:10 +0100

    AoC 2023 - day13 part I

--- day13.py
+++ day13.py
@@ -1,115 +1,96 @@
 import os
 
-from intcode_computer import IntcodeComputer
+from aoc_tools import get_data
 
-debug_mode = False
-
-
-def interpret_screen(output, screen_map: dict):
-    for i in range(int(len(output) / 3)):
-        x = output[3 * i]
-        y = output[3 * i + 1]
-        tile_id = output[3 * i + 2]
-        screen_map[(x, y)] = tile_id
-        if tile_id == 0:
-            screen_map.pop((x, y))
-
-    return screen_map
+debug_part1 = True
+debug_part2 = False
 
 
-def print_screen(screen_map: dict):
-    if not debug_mode:
-        return
-    output = ""
-    for i in range(24):
-        for j in range(50):
-            if (j, i) in screen_map:
-                output += str(screen_map[(j, i)])
-            else:
-                output += ' '
+def check_symmetry(data, start):
+    idx = 0
+    first = start - idx - 1
+    second = start + idx
+    while second < len(data) and first >= 0:
+        if data[first] != data[second]:
+            return False
+        idx += 1
+        first = start - idx - 1
+        second = start + idx
 
-        output += '\n'
-    score = get_score(screen_map)
-
-    print(output)
-    print('Score: {}'.format(score))
+    return True
 
 
-def get_score(screen_map):
-    return screen_map.get((-1, 0))
+def find_symmetry(data):
+    prev = -1
+    for i in range(len(data)):
+        if data[i] == prev:
+            if check_symmetry(data, i):
+                return i
+        prev = data[i]
+    return 0
 
 
-def part1(data: str):
-    program = [int(x) for x in data.split(",")]
-    computer = IntcodeComputer()
-    computer.import_program(program)
-    screen_map = {}
-    computer.compute_while_possible()
-    interpret_screen(computer.output, screen_map)
-    print_screen(screen_map)
-    return count_blocks(screen_map)
+def read_to_numbers(data):
+    rows = []
+    cols = None
+    for line in data.splitlines():
+        new_row = 0
+        if cols is None:
+            cols = [0 for _ in range(len(line))]
+        for col in range(len(line)):
+
+            val = 0
+            if line[col] == '#':
+                val = 1
+            new_row = new_row * 2 + val
+            cols[col] = cols[col] * 2 + val
+        rows.append(new_row)
+    return cols, rows
 
 
-def part2(data: str):
-    program = [int(x) for x in data.split(",")]
-    program[0] = 2
-    computer = IntcodeComputer()
-    computer.import_program(program)
+def part1(data):
+    result = 0
+    for subdata in data.split("\n\n"):
+        cols, rows = read_to_numbers(subdata)
 
-    return win_game_return_score(computer)
+        sym_col = find_symmetry(cols)
+        sym_row = find_symmetry(rows)
 
+        incr = sym_row * 100 + sym_col
+        result += incr
 
-def count_blocks(screen_map):
-    return len(find_tile_type(screen_map, 2))
-
-
-def find_tile_type(screen_map, tile_type):
-    return [x for x in screen_map.items() if x[1] == tile_type]
+    return result
 
 
-def win_game_return_score(computer):
-    computer.compute_while_possible()
-    screen_map = {}
-    interpret_screen(computer.output, screen_map)
-    print_screen(screen_map)
-    previous_paddle_x, previous_ball = find_paddle_and_ball(screen_map)
-    if debug_mode:
-        print("Paddle: {}, ball: {}".format(previous_paddle_x, previous_ball))
-
-    joystick_value = 0
-
-    while count_blocks(screen_map) > 0:
-        computer.accept_input([joystick_value])
-        computer.reset_output()
-        computer.compute_while_possible()
-        interpret_screen(computer.output, screen_map)
-        print_screen(screen_map)
-
-        # decision
-        paddle, ball = find_paddle_and_ball(screen_map)
-        if paddle > ball[0]:
-            joystick_value = -1
-        elif paddle < ball[0]:
-            joystick_value = 1
-
-    return get_score(screen_map)
+def part2(data):
+    pass
 
 
-def find_paddle_and_ball(screen_map):
-    paddle = max([int(coord[0]) for coord, _ in find_tile_type(screen_map, 3)])
-    ball, _ = find_tile_type(screen_map, 4)[0]
-    return paddle, ball
+def do_tests():
+    testdata1 = """#.##..##.
+..#.##.#.
+##......#
+##......#
+..#.##.#.
+..##..##.
+#.#.##.#.
 
-
-def read_data():
-    with open(input_filename) as input_file:
-        return input_file.read()
+#...##..#
+#....#..#
+..##..###
+#####.##.
+#####.##.
+..##..###
+#....#..#
+"""
+    print(part1(testdata1))
+    print(part2(testdata1))
 
 
 if __name__ == "__main__":
-    this_filename = os.path.basename(__file__)
-    input_filename = os.path.join("input", this_filename.replace("day", "").replace(".py", ".txt"))
-    data = read_data()
+    input_data = get_data(os.path.basename(__file__))
 
-    print(part1(data))
-    print(part2(data))
+    do_tests()
+
+    print(part1(input_data))
+    # print(part2(input_data))
