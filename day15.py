84fb311cd186b316ebf836f0e0553478656a7bbf
Jan Princ <jan.lopata@rohlik.cz>
2023-12-15 12:37:49 +0100

    AoC 2023 - day15 part II

--- day15.py
+++ day15.py
@@ -23,9 +23,83 @@ def part1(data):
     return result
 
 
+def add_to_box(list_to_add, label, power):
+    found_same = False
+    for i in range(len(list_to_add)):
+        current_lens = list_to_add[i]
+        if current_lens.label == label:
+            found_same = True
+            list_to_add[i] = Lens(label, power)
+
+    if not found_same:
+        list_to_add.append(Lens(label, power))
+
+
+def remove_from_box(list_to_remove_from, label):
+    remaining = []
+    for i in range(len(list_to_remove_from)):
+
+        current_lens = list_to_remove_from[i]
+        if current_lens.label != label:
+            remaining.append(current_lens)
+
+    list_to_remove_from.clear()
+    for x in remaining:
+        list_to_remove_from.append(x)
+
+    pass
+
+
+def compute_box_holding_factor(box):
+    if len(box) == 0:
+        return 0
+
+    result = 0
+    for i in range(len(box)):
+        lens = box[i]
+        result += (i + 1) * lens.power
+
+    return result
+
+
+def compute_score(boxes):
+    sum = 0
+    for i in range(len(boxes)):
+        box_n = i + 1
+        factor = compute_box_holding_factor(boxes[i])
+        sum += box_n * factor
+
+    return sum
+
 
 def part2(data):
-    pass
+    boxes = [[] for _ in range(256)]
+
+    for inp in data.strip().split(","):
+        hsh = compute_hash(inp)
+
+        if '=' in inp:
+            split = inp.split("=")
+            label = split[0]
+            label_hsh = compute_hash(label)
+            add_to_box(list_to_add=boxes[label_hsh], label=label, power=split[1])
+
+        if inp.endswith("-"):
+            label = inp[:-1]
+            label_hsh = compute_hash(label)
+            remove_from_box(list_to_remove_from=boxes[label_hsh], label=label)
+
+    return compute_score(boxes)
+
+
+class Lens:
+
+    def __init__(self, label, power):
+        self.label = label
+        self.power = int(power)
+
+    def __repr__(self):
+        return "{} {}x".format(self.label, self.power)
 
 
 def do_tests():
