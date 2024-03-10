9b8447ed2bc3e5d2091927b7da6a1084c1c6d051
Jan Princ <jan.lopata@rohlik.cz>
2024-03-10 17:10:50 +0100

    AoC 2023 - day 23 part II WIP

--- day23.py
+++ day23.py
@@ -7,6 +7,11 @@ debug_part1 = False
 debug_part2 = False
 
 SLOPE_MAP = {">": (0, 1), "<": (0, -1), "^": (-1, 0), "v": (1, 0)}
+DIRS = SLOPE_MAP.values()
+
+
+def plus_2d(pos, delta):
+    return pos[0] + delta[0], pos[1] + delta[1]
 
 
 def read_grid(data):
@@ -25,7 +30,10 @@ def read_maze(data):
             result[(i, j)] = line[j]
         i += 1
 
-    return result
+    max_i = max([x[0] for x in result.keys()])
+    max_j = max([x[1] for x in result.keys()])
+
+    return result, (max_i, max_j)
 
 
 def add_one_direction(visited: set, pos, delta, go_queue: queue):
@@ -74,7 +82,8 @@ def find_next_steps(visited: set, maze_map: dict, pos_i, pos_j):
         return slopes
 
 
-def print_maze(maze_map, max_i, max_j, visited):
+def print_maze(maze_map, maze_dimensions, visited):
+    max_i, max_j = (maze_dimensions[0], maze_dimensions[1])
     if not debug_part1:
         return
     for i in range(max_i + 1):
@@ -92,15 +101,11 @@ def print_maze(maze_map, max_i, max_j, visited):
 
 
 def part1(data):
-    maze_map = read_maze(data)
-
-    max_i = max([x[0] for x in maze_map.keys()])
-    max_j = max([x[1] for x in maze_map.keys()])
-
-    print_maze(maze_map, max_i, max_j, set())
+    maze_map, maze_dimensions = read_maze(data)
+    print_maze(maze_map, maze_dimensions, set())
 
     start_point = (0, 1)
-    end_point = (max_i, max_j - 1)
+    end_point = (maze_dimensions[0], maze_dimensions[1] - 1)
 
     go_queue = queue.Queue()
     go_queue.put((set(), start_point, (0, 0), start_point))
@@ -120,7 +125,7 @@ def part1(data):
             fun = max(fun, fun_here)
             if debug_part1:
                 print("end reached in {} steps".format(fun_here))
-                print_maze(maze_map, max_i, max_j, visited)
+                # print_maze(maze_map, max_i, max_j, visited)
 
         next_steps = find_next_steps(visited, maze_map, pos_i, pos_j)
 
@@ -135,8 +140,90 @@ def part1(data):
 def part2(data):
     updated_data = data.replace("^", ".").replace("v", ".").replace("<", ".").replace(">", ".")
     # print(updated_data)
-    return part1(updated_data)
 
+    maze_map, maze_dimensions = read_maze(data)
+    start = (0, 1)
+    graph = convert_to_graph(maze_map, start)
+    print(graph)
+    for key in graph:
+        maze_map[key] = '*'
+    print_maze(maze_map, maze_dimensions, set())
+
+
+def add_edge(graph, source, target, distance):
+    if source not in graph:
+        graph[source] = {}
+
+    graph[source][target] = distance
+
+
+def apply_symmetry(graph):
+    edges_to_add = []
+    for source in graph:
+        edges_map = graph[source]
+        for target in edges_map:
+            edges_to_add.append((target, source, edges_map[target]))
+
+    for source, target, distance in edges_to_add:
+        add_edge(graph, source, target, distance)
+
+
+def convert_to_graph(maze_map, start_point):
+    graph = {start_point: {}}
+    visited = set()
+    visited.add(start_point)
+    go_queue = queue.Queue()
+    go_queue.put((start_point, start_point))
+
+    while not go_queue.empty():
+        element = go_queue.get()
+        start = element[0]
+        previous_for_graph = element[1]
+        next_position, distance = crawl_to_next(visited, start, maze_map)
+        if next_position is not None:
+            # maze_map[next_position] = '*'
+            add_edge(graph, previous_for_graph, next_position, distance)
+            next_starts = find_unvisited_allowed(maze_map, visited, next_position)
+            for next_start in next_starts:
+                go_queue.put((next_start, next_position))
+
+    apply_symmetry(graph)
+    return graph
+
+
+def find_unvisited_allowed(maze_map, visited, start):
+    result = []
+    for delta in DIRS:
+        target = plus_2d(start, delta)
+
+        if target in visited:
+            continue
+
+        if target not in maze_map:
+            continue
+
+        if maze_map[target] == '#':
+            continue
+
+        result.append(target)
+    return result
+
+
+def crawl_to_next(visited, start, maze_map):
+    # should return next interesting point and distance to it
+    visited.add(start)
+
+    distance = 0
+    next_step = None
+    next_steps = find_unvisited_allowed(maze_map, visited, start)
+    while len(next_steps) == 1:
+        visited.add(start)
+        distance += 1
+        next_step = next_steps[0]
+        visited.add(next_step)
+        next_steps = find_unvisited_allowed(maze_map, visited, next_step)
+
+    return next_step, distance
 
 
 def do_tests():
