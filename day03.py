84b9cc1a3addc9bc8dceef283e16c633f7240be5
Jan Princ <lopata.jan@gmail.com>
2025-12-03 16:19:38 +0100

    Day 3

--- day03.py
+++ day03.py
@@ -1,65 +1,70 @@
 import os
-from functools import reduce
+
+from aoc_tools import get_data
 
 
-def part1(data: str):
-    lines = data.split("\n")
-    first_wire_dict = get_wire_path_dict(lines[0])
-    second_wire_dict = get_wire_path_dict(lines[1])
-    return reduce(lambda x, y: min(x, y),
-                  map(lambda pos: abs(pos[0]) + abs(pos[1]),
-                      set(first_wire_dict.keys()).intersection(set(second_wire_dict.keys()))
-                      ))
+def find_largest_digit(data):
+    mx = 0
+    arg_max = 0
+    for i in range(len(data)):
+        if data[i] > mx:
+            mx = data[i]
+            arg_max = i
+
+    return arg_max, mx
 
 
-def part2(data: str):
-    lines = data.split("\n")
-    first_wire_dict = get_wire_path_dict(lines[0])
-    second_wire_dict = get_wire_path_dict(lines[1])
-    return reduce(lambda x, y: min(x, y),
-                  map(lambda pos: first_wire_dict[pos] + second_wire_dict[pos],
-                      set(first_wire_dict.keys()).intersection(set(second_wire_dict.keys()))
-                      ))
+def find_largest_2digit(line: str):
+    data = [int(x) for x in line]
+
+    position, first_digit = find_largest_digit(data[:-1])
+    position2, second_digit = find_largest_digit(data[position + 1:])
+
+    return first_digit * 10 + second_digit
 
 
-def get_wire_path_dict(csv):
-    x = 0
-    y = 0
-    res = {}
-    step = 0
+def find_largest_12digit(line: str):
+    data = [int(x) for x in line]
 
-    for instruction in csv.split(","):
-        direction = instruction[0]
-        value = int(instruction[1:])
-        xdiff = 0
-        ydiff = 0
-        if direction == 'R':
-            xdiff = 1
-        if direction == 'L':
-            xdiff = -1
-        if direction == 'D':
-            ydiff = 1
-        if direction == 'U':
-            ydiff = -1
+    current_position = 0
+    result_stack = []
+    for i in range(11, -1, -1):
+        if i > 0:
+            target = data[current_position:-i]
+        else:
+            target = data[current_position:]
+        position_largest_digit, largest_digit = find_largest_digit(target)
+        next_position = current_position + position_largest_digit + 1
+        current_position = next_position
+        result_stack.append((current_position + position_largest_digit, largest_digit))
 
-        for i in range(value):
-            x += xdiff
-            y += ydiff
-            step += 1
-            res[(x, y)] = step
+    result = 0
+    for p, d in result_stack:
+        result = 10 * result + d
 
-    return res
+    return result
 
 
-def read_data():
-    with open(input_filename) as input_file:
-        return input_file.read()
+def part1(data):
+    result = 0
+    for line in data.splitlines():
+        result += find_largest_2digit(line)
+
+    return result
+
+
+def part2(data):
+    result = 0
+    for line in data.splitlines():
+        if len(line) < 2:
+            continue
+        result += find_largest_12digit(line)
+
+    return result
 
 
 if __name__ == "__main__":
-    this_filename = os.path.basename(__file__)
-    input_filename = os.path.join("input", this_filename.replace("day", "").replace(".py", ".txt"))
-    # print(input_filename)
+    input_data = get_data(os.path.basename(__file__))
 
-    print(part1(read_data()))
-    print(part2(read_data()))
+    print(part1(input_data))
+    print(part2(input_data))
