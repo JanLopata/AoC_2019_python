1bc35b64522a1fea3b017956a7955cba6eaa7709
Jan Princ <lopata.jan@gmail.com>
2025-12-05 20:08:12 +0100

    Day 5

--- day05.py
+++ day05.py
@@ -1,111 +1,98 @@
 import os
 
-
-def run_program(program: list, x_input, x_outputs):
-    program = [x for x in program]
-    pos = 0
-
-    while program[pos] != 99:
-
-        operation = program[pos] % 100
-
-        if operation == 1:
-            a = interpret_nth_param(program, pos, 1)
-            b = interpret_nth_param(program, pos, 2)
-            program[program[pos + 3]] = a + b
-            pos += 4
-
-        if operation == 2:
-            a = interpret_nth_param(program, pos, 1)
-            b = interpret_nth_param(program, pos, 2)
-            program[program[pos + 3]] = a * b
-            pos += 4
-
-        if operation == 3:
-            program[program[pos + 1]] = x_input
-            pos += 2
-
-        if operation == 4:
-            a = interpret_nth_param(program, pos, 1)
-            x_outputs.append(a)
-            pos += 2
-
-        if operation == 5:
-            a = interpret_nth_param(program, pos, 1)
-            if a != 0:
-                pos = interpret_nth_param(program, pos, 2)
-            else:
-                pos += 3
-
-        if operation == 6:
-            a = interpret_nth_param(program, pos, 1)
-            if a == 0:
-                pos = interpret_nth_param(program, pos, 2)
-            else:
-                pos += 3
-
-        if operation == 7:
-            a = interpret_nth_param(program, pos, 1)
-            b = interpret_nth_param(program, pos, 2)
-            program[program[pos + 3]] = int(a < b)
-            pos += 4
-
-        if operation == 8:
-            a = interpret_nth_param(program, pos, 1)
-            b = interpret_nth_param(program, pos, 2)
-            program[program[pos + 3]] = int(a == b)
-            pos += 4
-
-        if operation > 9:
-            raise ValueError
-
-    return program
+from aoc_tools import get_data
 
 
-def interpret_nth_param(program: list, position: int, param_number: int):
-    mode = mode_for_nth_parameter(program[position], param_number)
-
-    if mode == 0:
-        return program[program[position + param_number]]
-
-    if mode == 1:
-        return program[position + param_number]
-
-    raise ValueError
+def parse_range(line: str):
+    rs = line.split("-")
+    return int(rs[0]), int(rs[1])
 
 
-def mode_for_nth_parameter(code, n):
-    code = code // 100
-    divisor = 1
-    for i in range(n - 1):
-        divisor *= 10
-    return code // divisor % 10
+def parse_input(data):
+    ranges_processed = False
+    ranges = []
+    ids = []
+    for line in data.splitlines():
+        if line == "":
+            ranges_processed = True
+            continue
+
+        if ranges_processed:
+            ids.append(int(line))
+        else:
+            ranges.append(parse_range(line))
+
+    return ranges, ids
 
 
 def part1(data: str):
-    program = [int(x) for x in data.split(",")]
-    outputs = []
-    run_program(program, 1, outputs)
-    return outputs[-1]
+    ranges, ids = parse_input(data)
+
+    result = 0
+    for num in ids:
+        if is_number_in_any_range(num, ranges):
+            result += 1
+    return result
+
+
+def is_number_in_any_range(num, ranges):
+    for r in ranges:
+        if is_number_in_range(num, r):
+            return True
+    return False
+
+
+def is_number_in_range(num, r: tuple[int, int]):
+    return r[0] <= num <= r[1]
+
+
+def compute_range_overlaps(ranges):
+
+    pointers = []
+    for r in ranges:
+        pointers.append((r[0], r[1], "S"))
+        pointers.append((r[1], r[0], "E"))
+
+    pointers.sort(key=lambda pointer: pointer[1], reverse=True) # it is important to have the longer intervals first
+    pointers.sort(key=lambda pointer: pointer[0])
+
+    depth = 0
+    start = 0
+    end = 0
+    overlap_ranges = []
+    for p in pointers:
+        if p[2] == "S":
+            if depth == 0:
+                start = p[0]
+            depth += 1
+        else:
+            depth -= 1
+            if depth == 0:
+                end = p[0]
+                overlap_ranges.append((start, end))
+
+    return overlap_ranges
+
+
 
 
 def part2(data: str):
-    program = [int(x) for x in data.split(",")]
-    outputs = []
-    run_program(program, 5, outputs)
-    return outputs[-1]
+    ranges = []
+    for line in data.splitlines():
+        if line == "":
+            break
+        ranges.append(parse_range(line))
 
+    overlaps = compute_range_overlaps(ranges)
+    result = 0
+    for r in overlaps:
+        result += r[1] - r[0] + 1
 
-def read_data():
-    with open(input_filename) as input_file:
-        return input_file.read()
+    return result
 
 
 if __name__ == "__main__":
-    this_filename = os.path.basename(__file__)
-    input_filename = os.path.join("input", this_filename.replace("day", "").replace(".py", ".txt"))
-    data = read_data()
-    # print(input_filename)
+    input_data = get_data(os.path.basename(__file__))
 
-    print(part1(data))
-    print(part2(data))
+    print(part1(input_data))
+    print(part2(input_data))
--- test05.py
+++ test05.py
@@ -1,33 +1,50 @@
 import unittest
 
-from parameterized import parameterized
+import day05
 
-import day05 as day
+test_data = """3-5
+10-14
+16-20
+12-18
+
+1
+5
+8
+11
+17
+32
+"""
+
+test_data_2 = """3-5
+10-14
+16-20
+17-17
+12-18
+8-8
+"""
+
+test_data_3 = """3-5
+10-10
+10-14
+16-20
+16-16
+12-18
+"""
 
 
 class AoCTest(unittest.TestCase):
 
-    @parameterized.expand([["1002,4,3,4,33", "1002,4,3,4,99"]])
-    def test_int_computer(self, program, expected):
-        program_input = [int(x) for x in program.split(",")]
-        program_result = ','.join([str(x) for x in day.run_program(program_input, 0, [])])
-        self.assertEqual(expected, program_result)
+    def test_part1(self):
+        self.assertEqual(3, day05.part1(test_data))
 
-    @parameterized.expand([[1, 0], [2, 1], [3, 0]])
-    def test_1002_parameter_modes(self, position, mode):
-        self.assertEqual(mode, day.mode_for_nth_parameter(1002, position))
+    def test_part2(self):
+        self.assertEqual(14, day05.part2(test_data))
 
-    @parameterized.expand([[-5, 999], [0, 999], [7, 999], [8, 1000], [9, 1001], [10, 1001], [231, 1001]])
-    def test_eight_comparator_program(self, input_value, output):
-        program_plain = """
-3,21,1008,21,8,20,1005,20,22,107,8,21,20,1006,20,31,
-1106,0,36,98,0,0,1002,21,125,20,4,20,1105,1,46,104,
-999,1105,1,46,1101,1000,1,20,4,20,1105,1,46,98,99
-        """
-        program_input = [int(x) for x in program_plain.replace("\n", "").split(",")]
-        output_values = []
-        day.run_program(program_input, input_value, output_values)
-        self.assertEqual(output, output_values[-1])
+    def test_part2_special(self):
+        self.assertEqual(15, day05.part2(test_data_2))
+
+    def test_part2_special_alt(self):
+        self.assertEqual(14, day05.part2(test_data_3))
 
 
 if __name__ == '__main__':
