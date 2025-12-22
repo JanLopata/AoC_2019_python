3bf3c63e34e8f0c822144f19e23ba94bd65b0b83
Jan Princ <lopata.jan@gmail.com>
2025-12-22 23:14:38 +0100

    Day 10 WIP

--- test10.py
+++ test10.py
@@ -1,98 +1,20 @@
 import unittest
 
-from parameterized import parameterized
+import day10
 
-import day10 as day
-
-mini_data = """
-#.........
-...#......
-...#..#...
-.####....#
-..#.#.#...
-.....#....
-..###.#.##
-.......#..
-....#...#.
-...#..#..#
+test_data = """[.##.] (3) (1,3) (2) (2,3) (0,2) (0,1) {3,5,4,7}
+[...#.] (0,2,3,4) (2,3) (0,4) (0,1,2) (1,2,3,4) {7,5,12,7,2}
+[.###.#] (0,1,2,3,4) (0,3,4) (0,1,2,4,5) (1,2) {10,11,11,5,10,5}
 """
-
-test_data = [
-    (8, """
-.#..#
-.....
-#####
-....#
-...##
-"""),
-    (33, """
-......#.#.
-#..#.#....
-..#######.
-.#.#.###..
-.#..#.....
-..#....#.#
-#..#....#.
-.##.#..###
-##...#..#.
-.#....####
-"""),
-]
-
-large_data = """
-.#..##.###...#######
-##.############..##.
-.#.######.########.#
-.###.#######.####.#.
-#####.##.#.##.###.##
-..#####..#.#########
-####################
-#.####....###.#.#.##
-##.#################
-#####.##.###..####..
-..######..##.#######
-####.##.####...##..#
-.#####..#.######.###
-##...#.##########...
-#.##########.#######
-.####.#.###.###.#.##
-....##.##.###..#####
-.#.#.###########.###
-#.#.#.#####.####.###
-###.##.####.##.#..##
-"""
-
-gcd_data = [(4, 6, 2), (1, 8, 1), (4, 16, 4), (-4, 2, 2), (4, -2, 2), (-4, -2, 2)]
 
 
 class AoCTest(unittest.TestCase):
+
     def test_part1(self):
-        for example in test_data:
-            expected = example[0]
-            input_data = example[1]
-            result = day.part1(input_data)
-            self.assertEqual(expected, result[0])
-
-        self.assertEqual(210, day.part1(large_data)[0])
-
-    def test_mini(self):
-        grid_data, grid_size = day.read_grid_to_set(mini_data)
-        visible_points = day.compute_visible_points((0, 0), set(grid_data), grid_size)
-        self.assertEqual(7, visible_points)
-
-    @parameterized.expand([
-        [4, 6, 2],
-        [1, 8, 1],
-        [4, 16, 4],
-        [-4, 2, 2],
-        [4, -2, 2],
-        [-4, -2, 2]])
-    def test_gcd(self, a, b, expected):
-        self.assertEqual(expected, day.gcd(a, b))
+        self.assertEqual(7, day10.part1(test_data))
 
     def test_part2(self):
-        result = day.part2(large_data)
-        self.assertEqual(802, result)
+        self.assertEqual(0, day10.part2(test_data))
 
 
 if __name__ == '__main__':
