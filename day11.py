0e25be2ee12c9721c3282002505f860bb4e2be0c
Jan Princ <lopata.jan@gmail.com>
2026-01-10 19:18:49 +0100

    Day 11 Part I

--- day11.py
+++ day11.py
@@ -1,125 +1,53 @@
 import os
 
-from intcode_computer import IntcodeComputer, StoppingCondition
+from aoc_tools import get_data
 
 
-class PaintingRobot:
+class Accumulator:
 
     def __init__(self):
-        self.debug_mode = False
-        self.direction_idx = 0
-        self.directions = [(0, -1), (-1, 0), (0, 1), (1, 0)]
-        self.position = (0, 0)
-        self.environment = set()
-        self.visited = set()
+        self.value = 0
 
-    def move(self):
-        direction = self.get_direction()
-        new_position = (self.position[0] + direction[0], self.position[1] + direction[1])
-        if self.debug_mode:
-            print("Moving {} -> {} by {}, dir. {}".format(self.position, new_position, direction, self.direction_idx))
-        self.position = new_position
-
-    def get_direction(self):
-        return self.directions[self.direction_idx]
-
-    def turn(self, turn_number):
-        increment = -1 if turn_number == 1 else 1
-        new_direction = (self.direction_idx + increment) % len(self.directions)
-        if self.debug_mode:
-            print("Turning to {} by {}".format(new_direction, turn_number))
-        self.direction_idx = new_direction
-
-    def paint(self, color_number):
-        if self.debug_mode:
-            print("Painting on {} to {}".format(self.position, color_number))
-        if color_number == 1:
-            self.environment.add(self.position)
-
-        if color_number == 0 and self.position in self.environment:
-            self.environment.remove(self.position)
-
-        self.visited.add(self.position)
-
-    def get_color_number(self):
-        return 1 if self.position in self.environment else 0
+    def increment(self):
+        self.value += 1
 
 
-def run_robot(computer, partial_output, robot):
-    stopping_condition = None
-    while stopping_condition != StoppingCondition.END_OF_PROGRAM:
-
-        stopping_condition = computer.compute_while_possible()
-        if stopping_condition == StoppingCondition.EXPECTING_INPUT:
-            # read color
-            computer.accept_input([robot.get_color_number()])
-
-        if stopping_condition == StoppingCondition.STOP_ON_OUTPUT:
-            partial_output.append(computer.output[-1])
-            if len(partial_output) == 1:
-                robot.paint(partial_output[0])
-            if len(partial_output) >= 2:
-                robot.turn(partial_output[1])
-                robot.move()
-                partial_output.clear()
+def read_graph(data):
+    graph = {}
+    for line in data.split("\n"):
+        sp = line.split(": ")
+        if len(sp) < 2:
+            continue
+        source = sp[0]
+        targets = [x for x in sp[1].split(" ")]
+        graph[source] = targets
+    return graph
 
 
-def init_robot(input_program):
-    program = [int(x) for x in input_program.split(",")]
-    computer = IntcodeComputer()
-    computer.import_program(program)
-    computer.set_stop_on_output(True)
-    # this encapsulation is bad
-    # - the intcode computer should be inside of robot
-    # - the environment should be outside of robot
-    robot = PaintingRobot()
-    return computer, robot
+def count_paths(graph, source, target, accumulator:Accumulator):
+    if source == target:
+        accumulator.increment()
+
+    if source not in graph:
+        return 0
+
+    for available_target in graph[source]:
+        count_paths(graph, available_target, target, accumulator)
 
 
-def max_size(tuples_list: list):
-    return max(abs(x) for sub_tuple in tuples_list for x in sub_tuple)
+def part1(data: str):
+    graph = read_graph(data)
+    acc = Accumulator()
+    count_paths(graph, "you", "out", acc)
+    return acc.value
 
 
-def print_environment(environment):
-    max_range = max_size(environment)
-    result = ""
-    for i in range(-max_range, max_range):
-        for j in range(-max_range, max_range):
-            c = 'X' if (j, i) in environment else ' '
-            result += c
-        result += "\n"
-
-    print(result)
-
-
-def part1(input_program: str):
-    computer, robot = init_robot(input_program)
-    partial_output = []
-
-    run_robot(computer, partial_output, robot)
-
-    return len(robot.visited)
-
-
-def part2(input_program: str):
-    computer, robot = init_robot(input_program)
-    robot.environment.add((0, 0))
-    partial_output = []
-
-    run_robot(computer, partial_output, robot)
-
-    print_environment(robot.environment)
-
-
-def read_data():
-    with open(input_filename) as input_file:
-        return input_file.read()
+def part2(data: str):
+    pass
 
 
 if __name__ == "__main__":
-    this_filename = os.path.basename(__file__)
-    input_filename = os.path.join("input", this_filename.replace("day", "").replace(".py", ".txt"))
-    data = read_data()
+    input_data = get_data(os.path.basename(__file__))
 
-    print(part1(data))
-    print(part2(data))
+    print(part1(input_data))
+    print(part2(input_data))
--- test11.py
+++ test11.py
@@ -1,32 +1,27 @@
 import unittest
 
-import day11 as day
+import day11
 
-test_data_part1 = """
-109,2000,109,19,1102,1,102030,1985,204,-34,99;102030
-109,1,204,-1,1001,100,1,100,1008,100,16,101,1006,101,0,99;109,1,204,-1,1001,100,1,100,1008,100,16,101,1006,101,0,99
-1102,34915192,34915192,7,4,7,99,0;1219070632396864
-104,1125899906842624,99;1125899906842624
+test_data = """aaa: you hhh
+you: bbb ccc
+bbb: ddd eee
+ccc: ddd eee fff
+ddd: ggg
+eee: out
+fff: out
+ggg: out
+hhh: ccc fff iii
+iii: out
 """
-
-
-def max_size(data: list):
-    return max(abs(x) for sublist in data for x in sublist)
 
 
 class AoCTest(unittest.TestCase):
 
-    def test_max_size(self):
-        data = [(-20, 4), (-24, 12), (21, 6), (0, 7), (-3, -4)]
-        self.assertEqual(24, max_size(data))
-
     def test_part1(self):
-        for line in test_data_part1.splitlines():
-            if line == "":
-                continue
-            result = day.part1(line.split(";")[0])
-            interpreted_result = ",".join(str(it) for it in result)
-            self.assertEqual(line.split(";")[1], interpreted_result)
+        self.assertEqual(5, day11.part1(test_data))
+
+    def test_part2(self):
+        self.assertEqual(33, day11.part2(test_data))
 
 
 if __name__ == '__main__':
