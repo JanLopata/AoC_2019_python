f899f133f4b4ae4bc9c717bca5b2d7e4d03bb05f
Jan Princ <lopata.jan@gmail.com>
2026-01-11 15:48:51 +0100

    Day 11 Part II memoized

--- day11.py
+++ day11.py
@@ -3,33 +3,21 @@ import os
 from aoc_tools import get_data
 
 
-class Accumulator:
-
+class PathCounter:
     def __init__(self):
-        self.value = 0
-        self.filtered_value = 0
-        self.dac = 0
-        self.fft = 0
+        self.memory = {}
 
-    def increment(self):
-        self.value += 1
-        if self.dac > 0 and self.fft > 0:
-            self.filtered_value += 1
+    def memoize(self, source, path_count):
+        self.memory[source] = path_count
 
-    def mark_visit(self, node):
-        if node == "dac":
-            self.dac += 1
-        if node == "fft":
-            self.fft += 1
+    def is_memoized(self, source):
+        return source in self.memory.keys()
 
-    def mark_un_visit(self, node):
-        if node == "dac":
-            self.dac -= 1
-        if node == "fft":
-            self.fft -= 1
-
-    def __str__(self):
-        return "v: {} fv: {} dac: {} fft: {}".format(self.value, self.filtered_value, self.dac, self.fft)
+    def get_path_count(self, source):
+        if self.is_memoized(source):
+            return self.memory[source]
+        else:
+            return 0
 
 
 def read_graph(data):
@@ -44,31 +32,46 @@ def read_graph(data):
     return graph
 
 
-def count_paths(graph, source, target, accumulator: Accumulator):
+def count_paths(graph, source, target, path_counter: PathCounter):
+    if path_counter.is_memoized(source):
+        return path_counter.get_path_count(source)
+
     if source == target:
-        accumulator.increment()
+        path_counter.memoize(source, 1)
+        return 1
 
     if source not in graph:
         return 0
 
     for available_target in graph[source]:
-        accumulator.mark_visit(available_target)
-        count_paths(graph, available_target, target, accumulator)
-        accumulator.mark_un_visit(available_target)
+        count_paths(graph, available_target, target, path_counter)
+
+    path_count = sum([path_counter.get_path_count(target) for target in graph[source]])
+    path_counter.memoize(source, path_count)
+
+    return path_count
 
 
 def part1(data: str):
     graph = read_graph(data)
-    acc = Accumulator()
-    count_paths(graph, "you", "out", acc)
-    return acc.value
+    path_counter = PathCounter()
+    count_paths(graph, "you", "out", path_counter)
+    return path_counter.get_path_count("you")
 
 
 def part2(data: str):
     graph = read_graph(data)
-    acc = Accumulator()
-    count_paths(graph, "svr", "out", acc)
-    return acc.filtered_value
+
+    c1 = PathCounter()
+    count_paths(graph, "svr", "fft", c1)
+
+    c2 = PathCounter()
+    count_paths(graph, "fft", "dac", c2)
+
+    c3 = PathCounter()
+    count_paths(graph, "dac", "out", c3)
+
+    return c1.get_path_count("svr") * c2.get_path_count("fft") * c3.get_path_count("dac")
 
 
 if __name__ == "__main__":
