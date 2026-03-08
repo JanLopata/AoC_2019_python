4a8e45661776e46066a37f6bc5795a6fbfebd8e7
Jan Princ <lopata.jan@gmail.com>
2026-03-08 18:47:17 +0100

    Day 12 Part I introduce PreparedPiece object

--- day12.py
+++ day12.py
@@ -5,10 +5,93 @@ from aoc_tools import get_data
 debug_mode = False
 
 
-def parse_piece(part: str) -> tuple[int, set]:
+class Piece:
+
+    def __init__(self, indices_set: set[tuple[int, int]], number=0):
+        self.source_indices = indices_set
+        dim_i, dim_j = self.compute_dimensions()
+        self.dim_i = dim_i
+        self.dim_j = dim_j
+        self.number = number
+
+    def compute_dimensions(self):
+        max_i = max([x[0] for x in self.source_indices])
+        max_j = max([x[1] for x in self.source_indices])
+        return max_i, max_j
+
+    def get_indices(self):
+        return self.source_indices
+
+    def rotate_right(self):
+        rotated = set()
+        for i, j in self.source_indices:
+            rotated.add((j, self.dim_i - i))
+        return Piece(rotated)
+
+    def flip_top_down(self):
+        flipped = set()
+        for i, j in self.source_indices:
+            flipped.add((self.dim_i - i, j))
+        return Piece(flipped)
+
+    def hash_repr(self):
+        result = 0
+        for i in range(self.dim_i + 1):
+            for j in range(self.dim_j + 1):
+                result = result << 1
+                if (i, j) in self.source_indices:
+                    result += 1
+        return result
+
+    # debug method
+    def str_multiline(self):
+        lines = []
+        for i in range(self.dim_i + 1):
+            line = "".join(['#' if (i, j) in self.source_indices else '.' for j in range(self.dim_j + 1)])
+            lines.append(line)
+        return "\n".join(lines)
+
+    # debug method
+    def print(self):
+        print(self.str_multiline())
+
+
+class PreparedPiece:
+
+    def __init__(self, piece: Piece):
+        self.original_piece = piece
+        self.unique_variants = []
+        self.generate_variants()
+
+    def generate_variants(self):
+        all_variants = []
+        current_piece = self.original_piece
+        for i in range(4):
+            all_variants.append(current_piece)
+            all_variants.append(current_piece.flip_top_down())
+            current_piece = current_piece.rotate_right()
+
+        unique_variants = self.get_unique_variants(all_variants)
+
+        self.unique_variants = unique_variants
+
+    @staticmethod
+    def get_unique_variants(variants_full):
+        known_hashed = set()
+        unique_variants = []
+        for variant in variants_full:
+            hash_repr = variant.hash_repr()
+            if hash_repr in known_hashed:
+                continue
+            unique_variants.append(variant)
+            known_hashed.add(hash_repr)
+        return unique_variants
+
+
+def parse_piece(part: str) -> tuple[int, PreparedPiece]:
     line_idx = -1
     num = None
-    result_set = set()
+    raw_set = set()
     for line in part.splitlines():
         if len(line) == 0:
             continue
@@ -17,10 +100,11 @@ def parse_piece(part: str) -> tuple[int, set]:
         else:
             for i in range(len(line)):
                 if line[i] == "#":
-                    result_set.add((line_idx, i))
+                    raw_set.add((line_idx, i))
+
         line_idx += 1
 
-    return num, result_set
+    return num, PreparedPiece(Piece(raw_set, num))
 
 
 def parse_requirements(part: str):
@@ -50,6 +134,7 @@ def parse_parts(line: str):
 
 def parse_data(data):
     pieces = []
+    requirements = None
     for part in data.split("\n\n"):
         if "x" in part:
             requirements = parse_requirements(part)
@@ -60,10 +145,21 @@ def parse_data(data):
     print(pieces)
     print(requirements)
 
+    return pieces, requirements
+
+
+def check_fit(r, pieces):
+    return False
+
 
 def part1(data: str):
-    parse_data(data)
-    pass
+    pieces, requirements = parse_data(data)
+    fit_count = 0
+    for r in requirements:
+        if check_fit(r, pieces):
+            fit_count += 1
+
+    return 0
 
 
 def part2(data: str):
--- test12.py
+++ test12.py
@@ -1,6 +1,8 @@
 import unittest
 
-from day12 import part1, part2
+from parameterized import parameterized
+
+from day12 import part1, part2, Piece, PreparedPiece
 
 test_data = """
 0:
@@ -38,6 +40,7 @@ test_data = """
 12x5: 1 0 1 0 3 2
 """
 
+
 class AoCTest(unittest.TestCase):
 
     def test_part1(self):
@@ -45,6 +48,25 @@ class AoCTest(unittest.TestCase):
 
     def test_part2(self):
         self.assertEqual(0, part2(test_data))
+
+    @parameterized.expand([
+        ({(0, 0), (2, 1), (3, 2)}, {(0, 3), (1, 1), (2, 0)}),
+        ({(0, 1), (1, 0), (2, 0), (2, 2), (3, 1)}, {(0, 1), (0, 2), (1, 0), (1, 3), (2, 1)})
+    ])
+    def test_right_rotation(self, original, expected):
+        self.assertEqual(expected, Piece(original).rotate_right().get_indices())
+
+    @parameterized.expand([
+        ({(0, 0), (2, 1), (3, 2)}, {(3, 0), (1, 1), (0, 2)}),
+        ({(0, 1), (1, 0), (2, 0), (2, 2), (3, 1)}, {(0, 1), (1, 0), (2, 0), (1, 2), (3, 1)})
+    ])
+    def test_top_down_flip(self, original, expected):
+        self.assertEqual(expected, Piece(original).flip_top_down().get_indices())
+
+    def test_variants(self):
+        prepared_piece = PreparedPiece(Piece({(0, 0), (2, 1), (3, 2)}))
+
+
 
 
 if __name__ == '__main__':
